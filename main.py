a86dcb45129d08443fe17d6403ad4c6a9620eeff
204070 <talk2seun2010@hotmail.com>
2018-05-04 07:50:15 +0100

    Created templates

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 from flask import Flask,render_template, request
 app = Flask(__name__)
 
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import session
-# from database_setup import Base
-
-# engine = create_engine()
-# Base.metadata.bind = engine
-
-# DBSession = sessionmaker(bind=engine)
-# session = DBSession()
+# Add cloud datastore settings
 
 @app.route('/')
 def HelloWorld():
     return render_template("index.html")
 
+# login required
 @app.route('/profile/')
 def profile():
-    return 'This is my Profile Page'
+    return render_template('profile.html')
 
 @app.route('/submit-image')
 def image_submit():
-    return 'Form for submitting images'
+    return render_template('image-submit.html')
 
 @app.route('/image/<int:image_id>')
 def image_detail(image_id):
-    return "This is the page for image with id: " + str(image_id)
+    return render_template('image-detail.html', image_id=image_id)
 
 @app.route('/image/<int:image_id>/delete')
 def image_delete(image_id):
-    return "Delete image with id " + str(image_id)
+    return render_template('image-delete.html', image_id=image_id)
+
+
+def shorten(image_full_url):
+    pass
+
+def save_to_onedrive():
+    pass
 
 if __name__ == '__main__':
     app.run(host = '127.0.0.1', port=5000, debug=True)
